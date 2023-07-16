0620046b9c16353f6cda0d2e4fc3d9dbb6f24558
Jasonchen1203 <71620462+JasonChen1203@users.noreply.github.com>
2023-07-16 19:54:59 -0400

    wmt

---
+++ src/training_utils/preprocess_data.py
@@ -0,0 +1,18 @@
+import datasets
+from datasets import inspect_dataset, load_dataset_builder
+
+inspect_dataset("wmt14", "path/to/scripts")
+builder = load_dataset_builder(
+    "path/to/scripts/wmt_utils.py",
+    language_pair=("fr", "de"),
+    subsets={
+        datasets.Split.TRAIN: ["commoncrawl_frde"],
+        datasets.Split.VALIDATION: ["euelections_dev2019"],
+    },
+)
+
+# Standard version
+builder.download_and_prepare()
+ds = builder.as_dataset()
+
+print(ds)
