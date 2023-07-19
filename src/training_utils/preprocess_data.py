23ac25a5593a497d6957e38e1a6e9882b2c47a8f
Jasonchen1203 <71620462+JasonChen1203@users.noreply.github.com>
2023-07-18 23:08:11 -0400

    pkl

--- src/training_utils/preprocess_data.py
+++ src/training_utils/preprocess_data.py
@@ -1,8 +1,161 @@
 
-import tensorflow_datasets as tfds
+# import tensorflow_datasets as tfds
+# import os
+
+# data_dir = os.getcwd() + '\datasets'
+# print(data_dir)
+
+# ds = tfds.load('huggingface:wmt14/de-en', split='train', data_dir=data_dir, download=False)
+
+
+from pickle import dump, load
+from collections import Counter
+from turtle import update
+import numpy as np
+import re
+import string
+import unicodedata
 import os
 
-data_dir = os.getcwd() + '\datasets'
-print(data_dir)
+DATA_DIR = os.getcwd() + '\data'
 
-ds = tfds.load('huggingface:wmt14/de-en', split='train', data_dir=data_dir, download=False)
+# get file content
+def get_file_content(filename):
+    file = open(filename, mode='rt', encoding='utf-8')
+    file_content = file.read()
+    file.close()
+
+    return file_content
+
+
+# convert file content into lists
+def get_sentences(file_content):
+    return file_content.strip().split('\n')
+
+# get the longest and shortest sentence length
+def get_sentences_length(sentences):
+    max_len, min_len = -np.inf, np.inf
+
+    for sentence in sentences:
+        if len(sentence) > max_len: max_len = len(sentence)
+        elif len(sentence) < min_len: min_len = len(sentence)
+
+    return max_len, min_len
+
+
+def get_clean_sentences(sentences):
+        clean_sentences = []
+        # get regex pattern for printable chars
+        re_print = re.compile('[^%s]' % re.escape(string.printable))
+        # table for removing punctuation
+        punctation_table = str.maketrans('', '', string.punctuation)
+        
+        for sentence in sentences:
+                # normalize unicode characters
+                sentence = unicodedata.normalize('NFD', sentence).encode('ascii', 'ignore')
+                sentence = sentence.decode('UTF-8')
+                
+                # split sentence into words separated by whitespace
+                sentence = sentence.split()
+
+                for word in sentence:
+                    # convert to lower case + remove punctation
+                    word = word.lower().translate(punctation_table)
+
+                    # remove non-printable chars
+                    word = re_print.sub('', word)
+
+                # remove all words that contains numbers
+                sentence = [word for word in sentence if word.isalpha()]
+                # add space to the cleaned words
+                clean_sentences.append(' '.join(sentence))
+        
+        return clean_sentences
+
+
+def get_dir(filename):
+    return DATA_DIR + "\\" + filename
+
+
+# load dataset from pkl file into list
+def load_clean_sentences(filename):
+    return load(open(get_dir(filename), 'rb'))
+
+
+# get all vocab above a certain occurrence number into a list
+def get_vocab(sentences, min_occurrence):
+    vocab = Counter()
+
+    # get all vocab
+    for sentence in sentences:
+        word = sentence.split()
+        vocab.update(word)
+    
+    # remove vocab with low occurrence
+    updated_tokens = [k for k,c in vocab.items() if c >= min_occurrence]
+    return vocab, set(updated_tokens)
+
+
+# mark all out-of-vocab words with "unk" token
+def mark_OOV(sentences, vocab):
+    new_sentences = []
+    for sentence in sentences:
+        new_sentence = []
+        for token in sentence.split():
+                if token in vocab:
+                    new_sentence.append(token)
+                else:
+                    new_sentence.append('unk')
+        new_sentence = ' '.join(new_sentence)
+        new_sentences.append(new_sentence)
+
+    return new_sentences
+
+ 
+# save a list of clean sentences to pkl file
+def save_clean_sentences(sentences, filename):
+    dump(sentences, open(get_dir(filename), 'wb'))
+    print('Saved: %s' % filename)
+
+
+
+# load raw data files into .pkl files
+def get_pkl_file(data_filename, output_filename):
+    file_content = get_file_content(get_dir(data_filename))
+    sentences = get_sentences(file_content)
+    
+    max_len, min_len = get_sentences_length(sentences)
+    print(data_filename + ' Data INFO: sentences=%d, min=%d, max=%d' % (len(sentences), min_len, max_len))
+    
+    clean_sentences = get_clean_sentences(sentences)
+    
+    # get pkl file from the cleaned sentences
+    output_file = open(get_dir(output_filename), 'wb')
+    dump(clean_sentences, output_file)
+
+    output_file.close()
+    print(output_filename," saved")
+
+
+# further process the data and get finalized .pkl files
+def get_finalized_pkl_file(pkl_filename):
+    sentences = load_clean_sentences(pkl_filename)
+    
+    # process the vocab size
+    all_vocab, updated_vocab = get_vocab(sentences, min_occurrence=5)
+    print('All Vocabulary Size: %d' % len(all_vocab))
+    print('Updated Vocabulary Size: %d' % len(updated_vocab))
+
+    # mark OOV words & save the updated sentences into .pkl files
+    updated_sentences = mark_OOV(sentences, updated_vocab)
+    output_pkl_filename = pkl_filename.split('.')[0] + '_updated.pkl'
+    save_clean_sentences(updated_sentences, output_pkl_filename)
+
+    # print head of updated sentences
+    for i in range(20):
+        print("line",i,":",updated_sentences[i])
+    
+
+
+get_finalized_pkl_file("german.pkl")
+get_finalized_pkl_file("english.pkl")
