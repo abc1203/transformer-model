dc006c50951057bed33d647d740919cbf4e09f1e
Jasonchen1203 <71620462+JasonChen1203@users.noreply.github.com>
2023-07-24 15:45:24 -0400

    train

--- src/training/TrainTransformer.py
+++ src/training/TrainTransformer.py
@@ -1,9 +1,10 @@
+from statistics import mean
 import sys
 import os
 sys.path.insert(0, os.getcwd() +'\src\model')
 
 import tensorflow as tf
-from tensorflow import data, math, cast, float32, one_hot, shape
+from tensorflow import data, math, cast, float32, one_hot, shape, convert_to_tensor
 from tensorflow.keras.losses import CategoricalCrossentropy
 from Transformer import Transformer
 from LoadData import LoadData
@@ -33,7 +34,7 @@ class TrainTransformer:
         self.AdamOptimizer = AdamOptimizer()
 
         # load loss function with label smoothing of epsilon = 0.1
-        self.LossFunction = CategoricalCrossentropy(from_logits=True, label_smoothing=0.1)
+        self.LossFunction = CategoricalCrossentropy(from_logits=False, label_smoothing=0.1, reduction="none")
 
         print("Transformer Initialized")
         print("=======================================================================================================")
@@ -44,20 +45,49 @@ class TrainTransformer:
         # shape(mask_mat) = (batch_size, seq_len)
         mask_mat = math.logical_not(math.equal(target, 0))
         mask_mat = cast(mask_mat, float32)
-        print(shape(mask_mat))
 
         # apply one-hot encoding on target
-        # shape(target) = (batch_size, seq_len, vocab_size)
-        target = one_hot(target, depth=target_vocab_size)
-        loss = self.LossFunction(target, pred)
+        # shape(target_one_hot) = (batch_size, seq_len, vocab_size)
+        # shape(loss) = (batch_size, seq_len)
+        target_one_hot = one_hot(target, depth=target_vocab_size)
+        loss = self.LossFunction(target_one_hot, pred)
+
+        # apply masking 
+        loss_masked = []
+        for i in range(loss.shape[0]):
+            loss_item = [loss[i][j] * mask_mat[i][j] for j in range(loss.shape[1])]
+            loss_masked.append(loss_item)
+        loss_masked = convert_to_tensor(loss_masked)
+
+        # calculate mean loss
+        mean_loss = math.reduce_sum(loss_masked) / math.reduce_sum(mask_mat)
+        mean_loss = mean_loss.item()
+        print("Mean Loss in current batch: ", mean_loss)
+
+        return mean_loss
     
 
+    # graph execution
+    @tf.function
     def train_step(self, encoder_inputs, decoder_inputs, decoder_outputs):
-        pred = self.Transformer(encoder_inputs, decoder_inputs, is_training = True)
-        print(pred[0][pred.shape[1]-1])
-        print(math.reduce_sum(pred[0][pred.shape[1]-1]))
-        return pred
+        with tf.GradientTape() as tape:
+            pred = self.Transformer(encoder_inputs, decoder_inputs, is_training=True)
+            print(pred)
     
+            # get loss value
+            loss = self.get_loss(pred, decoder_outputs, self.decoder_vocab_size)
+        
+        print("Updating weights...")
+    
+        # get gradients from loss & update the trainable weights
+        gradients = tape.gradient(loss, self.Transformer.trainable_weights)
+        self.AdamOptimizer.apply_gradients(zip(gradients, self.Transformer.trainable_weights))
+
+        print("Done")
+        print("Step finished")
+        print("=============================================================================================")
+    
+
     def __call__(self):
         print("Starting Training: ")
         
@@ -67,15 +97,13 @@ class TrainTransformer:
             encoder_inputs = data_trainX[1:]
             decoder_inputs = data_trainY[:-1]
             decoder_outputs = data_trainY[1:]
-            print(decoder_outputs[0])
 
-            pred = self.train_step(encoder_inputs, decoder_inputs, decoder_outputs)
-
-            self.get_loss(pred, decoder_outputs, self.decoder_vocab_size)
+            self.train_step(encoder_inputs, decoder_inputs, decoder_outputs)
 
             break
 
         print("Training Complete")
+        print("=======================================================================================================")
 
 
 train_transformer = TrainTransformer('english_updated.pkl', 'german_updated.pkl')
--- src/training/test.py
+++ src/training/test.py
@@ -1,8 +1,23 @@
 import numpy as np
+from tensorflow import one_hot, shape, convert_to_tensor, math, cast, float32
+from time import time
 
 
-mat1 = np.random.random((2, 3, 5))
-mat2 = np.array([[1, 0, 0], [1, 1, 0]])
+mat1 = np.random.random((64, 100))
+mat2 = math.logical_not(math.equal(mat1, 0))
+mat2 = cast(mat2, float32)
 
-print(mat1)
-print(mat1 * mat2)
+mat1_one_hot = one_hot(mat1, depth=4000)
+print(mat1_one_hot)
+
+output_list = []
+
+start_time = time()
+
+for i in range(mat1_one_hot.shape[0]):
+    sentence = [mat1_one_hot[i][j] * mat2[i][j] for j in range(mat1_one_hot.shape[1])]
+    output_list.append(sentence)
+
+
+print(convert_to_tensor(output_list))
+print(math.reduce_sum(output_list) / math.reduce_sum(mat2))
